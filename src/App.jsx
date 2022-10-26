c23daac064274716c9f70fb2553cc3826775fefc
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2022-10-27 01:09:41 +0530

    style

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,35 @@ import Header from "./components/Header/header.jsx";
 import Input from "./components/Input/Input.jsx";
 import Card from "./components/cards/Cards.jsx"
 
-// import "./App.css"
+import "./App.css"
 
 function App(){
    return <div className="main-app-container">
      <Header/>
      <Input/>
-      <Card />
+     <div className="card-element-section" >
+        {/* {[1,2,3,4,5].map((curr)=><Card/>)} */}
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+
+     </div>
     
     
    </div>
