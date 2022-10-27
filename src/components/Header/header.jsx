2ffbd4f609f4d682821cd70c65c7bfdce007db0b
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2022-10-27 16:40:13 +0530

    style: icon added in the header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import CreateIcon from '@mui/icons-material/Create';
 import "./header.css";
 
 function Header(){
     return <>
         <header>
-            <h2>personal<span>Note</span></h2>
+            <h2>personal<span>Note</span><CreateIcon/></h2>
         </header>
     </>
 }
