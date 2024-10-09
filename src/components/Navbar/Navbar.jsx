93f4442093d275d60c83db9f99b24e73dbfe4998
Ravi Pratihast <ravipratihast@gmail.com>
2024-10-09 23:12:28 +0530

    Style changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { LiaFilterSolid } from "react-icons/lia";
+// import { MdDarkMode } from "react-icons/md";
+import { CiLogin } from "react-icons/ci";
+
 import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="navbar-links">
-        <li>Filter</li>
-        <li>Dark Mode</li>
-        <li>Login</li>
+        <li>
+          <LiaFilterSolid className="icon filter-icon" />
+        </li>
+        {/* <li>
+          <MdDarkMode className="icon" />
+        </li> */}
+        <li>
+          <CiLogin className="icon" />
+        </li>
       </ul>
     </nav>
   );
