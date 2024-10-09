4c425fcb73cb92cfceabab32c959bbce3dcbaa60
Ravi Pratihast <ravipratihast@gmail.com>
2024-10-09 21:03:53 +0530

    Add: Created and mounted search component with CSS styling

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import CreateIcon from '@mui/icons-material/Create';
+import CreateIcon from "@mui/icons-material/Create";
+import { Search, Navbar } from "../index-component";
 import "./header.css";
 
-function Header(){
-    return <>
-        <header>
-            <h2>personal<span>Note</span><CreateIcon/></h2>
-        </header>
-    </>
-}
+function Header() {
+  return (
+    <>
+      <header>
+        {/* The Logo */}
+        <h2>
+          personal<span>Note</span>
+          <CreateIcon />
+        </h2>
 
+        {/* Search Bar Component */}
+        <Search />
+        {/* Navigation bar -> that will have three items  */}
+        <Navbar />
+      </header>
+    </>
+  );
+}
 
 export default Header;
